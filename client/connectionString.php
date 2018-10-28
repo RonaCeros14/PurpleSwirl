b887b2225e7251eff678522dbab33f7d9027862b
RonaCeros14 <ilovatamhome14@gmail.com>
2018-10-28 18:45:27 +0800

    RonaSecondPush

--- client/connectionString.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$databaseHost = 'localhost';
-$databaseName = 'purpleswirldb';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$connect = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
-?>
